72d0076444138030d491db53c7eff4060ac163ce
Naveenbrusier12 <pandurebelsachin18@gmail.com>
2021-03-17 23:01:49 +0530

    Version 2.0

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
-var clearBut = document.getElementById('clear');
-var answer = document.getElementById('disp');
-var eqnString = '';
-var finalAnswer = 0;
-clearBut.onclick = function() {
-    answer.innerText = '';
-    finalAnswer = 0;
-    eqnString = '';
+var display = document.getElementById('disp');
+var num = document.getElementsByClassName("numbers");
+var ope = document.getElementsByClassName("operator");
+var eq = document.getElementById('equal');
+var operator1=0,operator2,operand = '+';
+var clear = document.getElementById('clear');
+var per = document.getElementById('percentage');
+for (let index = 0; index < num.length; index++) {
+    num[index].addEventListener('click',function (params) {
+        display.innerText += this.getAttribute('data-value');
+    })
 }
-var numbers = document.getElementsByClassName("numbers");
-// for(var element in numbers) {
-//     element.onclick = function (params) {
-//         answer.innerText = element.innerText;
-//     }
-// }
-[...numbers].forEach(i => {
-    i.onclick = function (params) {
-        answer.innerText += i.innerText;
-        eqnString += i.innerText;
-    }
-});
-
-var operators = document.getElementsByClassName('operator');
-[...operators].forEach(i => {
-    i.onclick = function (){
-        eqnString +=  i.innerText;
-        answer.innerText = ''
-    }
-})
-
-var evalu = document.getElementById('equal');
-evalu.onclick = function (params) {
-    //eqnString = answer.innerText;
-    finalAnswer = eval(eqnString);
-    answer.innerText = finalAnswer;
-    eqnString = ''+finalAnswer;
-    console.log(eqnString);
+// clearing the display
+clear.onclick = function (params) {
+    display.innerText = '';
 }
 
-var sign = document.getElementById('sign-alter');
-sign.onclick = function (params) {
-    answer.innerText = ( 0 - parseInt( answer.innerText));
+// performing the operations
+for (let index = 0; index < ope.length; index++) {
+    ope[index].addEventListener('click',function (params) {
+        operator1 = display.innerText ;
+        operand = this.getAttribute('data-value');
+        display.innerText = '';
+    })
+}
+
+//Calculating the result
+eq.onclick = function (params) {
+    operator2 = display.innerText;
+    console.log(operator1,operand,operator2);
+    let ans = eval(operator1 + operand + operator2);
+    if(ans == Infinity){
+        display.innerText = "Error";
+    }
+    else {
+        display.innerText = ans;
+        operator1 = 0;
+        operand = "+";
+    }
+}
+
+//calc the % 
+per.onclick = function (params) {
+    display.innerText /= 100;
+}
+
+//altering the sign
+var signAlter = document.getElementById('sign-alter');
+signAlter.onclick = function (params) {
+    display.innerText = 0 - display.innerText;
 }
